3c4e378f1d6f421901f4d6f86a1f8099051185e1
Rakesh <Rakesh@RMK-iMac5K-2.local>
2025-10-04 15:24:31 -0400

    Update ElkOutput.ts for Homebridge 2.0 compatibility

--- src/accessories/ElkOutput.ts
+++ src/accessories/ElkOutput.ts
@@ -1,79 +1,40 @@
-'use strict';
-import { Service, PlatformAccessory, CharacteristicValue } from 'homebridge';
+import { Service, Characteristic, PlatformAccessory } from 'homebridge';
 import { ElkM1Platform } from '../platform';
-import Elk from 'elkmon';
 
 export class ElkOutput {
+  private service: Service;
 
-    protected isOn = false;
-    private service: Service;
-    private elk: Elk;
-    private id: number;
+  constructor(
+    private readonly platform: ElkM1Platform,
+    private readonly accessory: PlatformAccessory,
+    private readonly output: any,
+  ) {
+    const { Service, Characteristic } = this.platform.api.hap;
 
-    constructor(
-        protected readonly platform: ElkM1Platform,
-        protected readonly accessory: PlatformAccessory,
-    ) {
+    this.accessory.getService(Service.AccessoryInformation)!
+      .setCharacteristic(Characteristic.Manufacturer, 'Elk Products')
+      .setCharacteristic(Characteristic.Model, 'M1 Output')
+      .setCharacteristic(Characteristic.SerialNumber, `Output-${output.outputNumber}`);
 
-        const device = accessory.context.device;
+    this.service = this.accessory.getService(Service.Switch) ||
+      this.accessory.addService(Service.Switch);
 
-        this.elk = device.elk;
-        this.id = device.id;
+    this.service.setCharacteristic(Characteristic.Name, output.name);
 
-        this.service = accessory.getService(platform.Service.Switch) ||
-            accessory.addService(platform.Service.Switch);
+    // Initial state
+    this.service.setCharacteristic(Characteristic.On, output.state);
 
-        /* this.contactCharacteristic = platform.Characteristic.ContactSensorState;
-        this.tamperCharacteristic = platform.Characteristic.StatusTampered;*/
+    // Listen for output changes
+    this.platform.elkClient.onOutputChange((updatedOutput: any) => {
+      if (updatedOutput.outputNumber === output.outputNumber) {
+        this.service.updateCharacteristic(Characteristic.On, updatedOutput.state);
+      }
+    });
 
-        // set accessory information
-        this.accessory.getService(this.platform.Service.AccessoryInformation)!
-            .setCharacteristic(this.platform.Characteristic.Model, 'Output');
-
-        const itemName = (typeof device.name !== 'undefined') ? device.name :
-            `Output ${device.id}`;
-
-        this.service.setCharacteristic(this.platform.Characteristic.Name, itemName);
-
-        this.service.getCharacteristic(this.platform.Characteristic.On)
-            .onGet(this.getOutput.bind(this))
-            .onSet(this.setOutput.bind(this));
-
-        this.elk.on('CC', (outputChange) => {
-            if (outputChange.id === this.id) {
-                const isOn = outputChange.state === 'On';
-                if (isOn !== this.isOn) {
-                    this.isOn = isOn;
-                    this.service.updateCharacteristic(this.platform.Characteristic.On, isOn);
-                }
-            }
-        });
-
-
-    }
-
-    async getOutput(): Promise<CharacteristicValue> {
-        return this.elk.requestOutputStatusReport().then((outputStatusResponse) => {
-            const isOn = outputStatusResponse.outputs[this.id - 1] === 'On';
-            this.isOn = isOn;
-            return isOn;
-        })
-            .catch((err) => {
-                this.platform.log.error(`Caught error (${err}) trying to get current state of output ${this.id}`);
-                return this.isOn;
-            });
-    }
-
-    async setOutput(value: CharacteristicValue) {
-        const newState = `${value}` === 'true';
-        this.platform.log.debug(`Setting output ${this.id} to ${value}  newState = ${newState} currentState = ${this.isOn}`);
-        if (newState !== this.isOn) {
-            if (newState) {
-                this.elk.setOutputOn(this.id, 0);
-            } else {
-                this.elk.setOutputOff(this.id);
-            }
-            this.isOn = newState;
-        }
-    }
+    // Handle HomeKit switch changes
+    this.service.getCharacteristic(Characteristic.On)
+      .onSet((value: boolean) => {
+        this.platform.elkClient.setOutputState(output.outputNumber, value);
+      });
+  }
 }
