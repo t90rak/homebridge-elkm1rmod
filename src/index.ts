4db7e71e03a664ab8444b1d6fe68a61bb1cb02e3
Rakesh <Rakesh@RMK-iMac5K-2.local>
2025-10-05 21:34:11 -0400

    Fixed platform registration to use correct Homebridge API

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import { API } from 'homebridge';
 
-import { PLATFORM_NAME } from './settings';
+import { PLATFORM_NAME, PLUGIN_NAME } from './settings';
 import { ElkM1Platform } from './platform';
 
 /**
  * This method registers the platform with Homebridge
  */
 export = (api: API) => {
-    api.registerPlatform(PLATFORM_NAME, ElkM1Platform);
+    api.registerPlatform(PLUGIN_NAME, PLATFORM_NAME, ElkM1Platform);
 };
